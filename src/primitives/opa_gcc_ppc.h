fa5882df99f2afe23f6ad8c228d3a03234e677d1
Dave Goodell <goodell@mcs.anl.gov>
2009-02-23 23:20:29 +0000

    [svn-r16] Update PPC (BG/P) atomic primitives to the new-style (OPA_int_t) interface. I have not had a chance to test these, so they are probably not quite right yet.
    No reviewer.

--- src/primitives/opa_gcc_ppc.h
+++ src/primitives/opa_gcc_ppc.h
@@ -4,29 +4,31 @@
  *      See COPYRIGHT in top-level directory.
  */
 
-/* FIXME needs to be converted to the new OPA_int_t-style of functions */
-
 #ifndef OPA_GCC_PPC_H_INCLUDED
 #define OPA_GCC_PPC_H_INCLUDED
+
+/* these need to be aligned on an 8-byte boundary to work on a BG/P */
+typedef struct { volatile int v;    ATTRIBUTE((aligned (8))) } OPA_int_t;
+typedef struct { void * volatile v; ATTRIBUTE((aligned (8))) } OPA_ptr_t;
 
 /*
    load-link/store-conditional (LL/SC) primitives.  We LL/SC implement
    these here, which are arch-specific, then use the generic
-   implementations from _atomic_emulated.h */
+   implementations from opa_emulated.h */
 
-static inline int OPA_LL_int(volatile int *ptr)
+static inline int OPA_LL_int(OPA_int_t *ptr)
 {
     int val;
     __asm__ __volatile__ ("lwarx %[val],0,%[ptr]"
                           : [val] "=r" (val)
-                          : [ptr] "r" (ptr)
+                          : [ptr] "r" (&ptr->v)
                           : "cc");
 
     return val;
 }
 
 /* Returns non-zero if the store was successful, zero otherwise. */
-static inline int OPA_SC_int(volatile int *ptr, int val)
+static inline int OPA_SC_int(OPA_int_t *ptr, int val)
 {
     int ret;
     __asm__ __volatile__ ("stwcx. %[val],0,%[ptr];\n"
@@ -34,7 +36,7 @@ static inline int OPA_SC_int(volatile int *ptr, int val)
                           "li %[ret], 0;\n"
                           "1: ;\n"
                           : [ret] "=r" (ret)
-                          : [ptr] "r" (ptr), [val] "r" (val), "0" (ret)
+                          : [ptr] "r" (&ptr->v), [val] "r" (val), "0" (ret)
                           : "cc", "memory");
     return ret;
 }
@@ -46,29 +48,23 @@ static inline int OPA_SC_int(volatile int *ptr, int val)
    differing word and pointer widths we can write separate versions.
 */
 
-static inline int *OPA_LL_int_ptr(volatile int **ptr)
+static inline int *OPA_LL_ptr(OPA_ptr_t *ptr)
 {
-    switch (sizeof(int *)) { /* should get optimized away */
-        case sizeof(int):
-            return OPA_LL_int((volatile int)ptr);
-            break;
-        default:
-            MPIU_Assertp(0); /* need to implement a separate ptr-sized version */
-            return NULL; /* placate the compiler */
-    }
+    /* need to implement a separate ptr-sized version, although it's not needed
+       on the BG/P it might be needed on a different PPC impl */
+    OPA_COMPILE_TIME_ASSERT(sizeof(int) == sizeof(void *)); 
+    OPA_COMPILE_TIME_ASSERT(sizeof(OPA_int_t) == sizeof(OPA_ptr_t));
+    return OPA_LL_int((OPA_int_t *)ptr);
 }
 
 /* Returns non-zero if the store was successful, zero otherwise. */
-static inline int OPA_SC_int_ptr(volatile int **ptr, int *val)
+static inline int OPA_SC_ptr(OPA_ptr_t *ptr, int *val)
 {
-    switch (sizeof(int *)) { /* should get optimized away */
-        case sizeof(int):
-            return OPA_SC_int((volatile int *)ptr, (int)val);
-            break;
-        default:
-            MPIU_Assertp(0); /* need to implement a separate ptr-sized version */
-            return NULL; /* placate the compiler */
-    }
+    /* need to implement a separate ptr-sized version, although it's not needed
+       on the BG/P it might be needed on a different PPC impl */
+    OPA_COMPILE_TIME_ASSERT(sizeof(int) == sizeof(void *)); 
+    OPA_COMPILE_TIME_ASSERT(sizeof(OPA_int_t) == sizeof(OPA_ptr_t));
+    return OPA_SC_int((OPA_int_t *)ptr, (int)val);
 }
 
 /* Implement all function using LL/SC */
@@ -80,11 +76,11 @@ static inline int OPA_SC_int_ptr(volatile int **ptr, int *val)
 #define OPA_fetch_and_add_by_llsc  OPA_fetch_and_add
 #define OPA_fetch_and_decr_by_llsc OPA_fetch_and_decr
 #define OPA_fetch_and_incr_by_llsc OPA_fetch_and_incr
-#define OPA_cas_int_ptr_by_llsc    OPA_cas_int_ptr
+#define OPA_cas_ptr_by_llsc        OPA_cas_ptr
 #define OPA_cas_int_by_llsc        OPA_cas_int
-#define OPA_swap_int_ptr_by_llsc   OPA_swap_int_ptr
+#define OPA_swap_ptr_by_llsc       OPA_swap_ptr
 #define OPA_swap_int_by_llsc       OPA_swap_int
 
-#include"mpidu_atomic_emulated.h"
+#include "opa_emulated.h"
 
 #endif /* OPA_GCC_PPC_H_INCLUDED */
