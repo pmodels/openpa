625cd7f568b57889ea8d15f1acdf5e7ebb270df4
Darius Buntinas <buntinas@mcs.anl.gov>
2009-03-23 22:00:33 +0000

    [svn-r23] removed superfluous; after macro definition

--- src/primitives/opa_nt_intrinsics.h
+++ src/primitives/opa_nt_intrinsics.h
@@ -96,9 +96,9 @@ static inline int OPA_swap_int(volatile int *ptr, int val)
 #define OPA_fetch_and_incr OPA_fetch_and_incr_by_faa
 #define OPA_fetch_and_decr OPA_fetch_and_decr_by_faa
 
-#define OPA_write_barrier()      _WriteBarrier();
-#define OPA_read_barrier()       _ReadBarrier();
-#define OPA_read_write_barrier() _ReadWriteBarrier();
+#define OPA_write_barrier()      _WriteBarrier()
+#define OPA_read_barrier()       _ReadBarrier()
+#define OPA_read_write_barrier() _ReadWriteBarrier()
 
 #include "opa_emulated.h"
 
