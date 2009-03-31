c09429717320de3be212e6cbe616c4b6f0cc9fe1
Darius Buntinas <buntinas@mcs.anl.gov>
2009-03-31 21:45:06 +0000

    [svn-r33] converted sun_atomic_ops to use new interface

--- src/primitives/opa_sun_atomic_ops.h
+++ src/primitives/opa_sun_atomic_ops.h
@@ -4,74 +4,98 @@
  *      See COPYRIGHT in top-level directory.
  */
 
-/* FIXME needs to be converted to the new OPA_int_t-style of functions */
 
 #ifndef OPA_SUN_ATOMIC_OPS_H_INCLUDED
 #define OPA_SUN_ATOMIC_OPS_H_INCLUDED
 
 #include <atomic.h>
 
-static inline void OPA_add(volatile int *ptr, int val)
+typedef struct { volatile uint_t v;  } OPA_int_t;
+typedef struct { void * volatile v; } OPA_ptr_t;
+
+static inline int OPA_load(OPA_int_t *ptr)
 {
-    atomic_add_int((volatile uint_t *)ptr, val);
+    return (int)ptr->v;
 }
 
-static inline void OPA_incr(volatile int *ptr)
+static inline void OPA_store(OPA_int_t *ptr, int val)
 {
-    atomic_inc_uint((volatile uint_t *)ptr);
+    ptr->v = (uint)val;
 }
 
-static inline void OPA_decr(volatile int *ptr)
+static inline void *OPA_load_ptr(OPA_ptr_t *ptr)
 {
-    atomic_dec_uint((volatile uint_t *)ptr);
+    return ptr->v;
 }
 
-
-static inline int OPA_decr_and_test(volatile int *ptr)
+static inline void OPA_store_ptr(OPA_ptr_t *ptr, void *val)
 {
-    return atomic_dec_uint_nv((volatile uint_t *)ptr) == 0;    
-}
-
-
-static inline int OPA_fetch_and_add(volatile int *ptr, int val)
-{
-    return (int)atomic_add_int_nv((volatile uint_t *)ptr, val) - val;
-}
-
-static inline int OPA_fetch_and_decr(volatile int *ptr)
-{
-    return (int)atomic_dec_uint_nv((volatile uint_t *)ptr) + 1;
-}
-
-static inline int OPA_fetch_and_incr(volatile int *ptr)
-{
-    return (int)atomic_inc_uint_nv((volatile uint_t *)ptr) - 1;
+    ptr->v = val;
 }
 
 
-static inline int *OPA_cas_int_ptr(int * volatile *ptr, int *oldv, int *newv)
+static inline void OPA_add(OPA_int_t *ptr, int val)
+{
+    atomic_add_int(&ptr->v, val);
+}
+
+static inline void OPA_incr(OPA_int_t *ptr)
+{
+    atomic_inc_uint(&ptr->v);
+}
+
+static inline void OPA_decr(OPA_int_t *ptr)
+{
+    atomic_dec_uint(&ptr->v);
+}
+
+
+static inline int OPA_decr_and_test(OPA_int_t *ptr)
+{
+    return atomic_dec_uint_nv(&ptr->v) == 0;    
+}
+
+
+static inline int OPA_fetch_and_add(OPA_int_t *ptr, int val)
+{
+    return (int)atomic_add_int_nv(&ptr->v, val) - val;
+}
+
+static inline int OPA_fetch_and_decr(OPA_int_t *ptr)
+{
+    return (int)atomic_dec_uint_nv(&ptr->v) + 1;
+}
+
+static inline int OPA_fetch_and_incr(OPA_int_t *ptr)
+{
+    return (int)atomic_inc_uint_nv(&ptr->v) - 1;
+}
+
+
+static inline int *OPA_cas_ptr(OPA_ptr_t *ptr, int *oldv, int *newv)
 {
     return atomic_cas_ptr(ptr, oldv, newv);
 }
 
-static inline int OPA_cas_int(volatile int *ptr, int oldv, int newv)
+static inline int OPA_cas_int(OPA_int_t *ptr, int oldv, int newv)
 {
-    return (int)atomic_cas_uint((volatile uint_t *)ptr, (uint_t)oldv, (uint_t)newv);
+    return (int)atomic_cas_uint(&ptr->v, (uint_t)oldv, (uint_t)newv);
 }
 
-static inline int *OPA_swap_int_ptr(int * volatile *ptr, int *val)
+
+static inline int *OPA_swap_ptr(OPA_ptr_t *ptr, int *val)
 {
     return atomic_swap_ptr(ptr, val);
 }
 
-static inline int OPA_swap_int(volatile int *ptr, int val)
+static inline int OPA_swap_int(OPA_int_t *ptr, int val)
 {
-    return (int)atomic_swap_uint((volatile uint_t *)ptr, (uint_t) val);
+    return (int)atomic_swap_uint(&ptr->v, (uint_t) val);
 }
 
-#define OPA_write_barrier()      membar_producer()
-#define OPA_read_barrier()       membar_consumer()
-#define OPA_read_write_barrier() do { membar_consumer(); membar_producer(); } while (0)
 
+#define MPIDU_Shm_write_barrier()      membar_producer()
+#define MPIDU_Shm_read_barrier()       membar_consumer()
+#define MPIDU_Shm_read_write_barrier() do { membar_consumer(); membar_producer(); } while (0)
 
 #endif /* OPA_SUN_ATOMIC_OPS_H_INCLUDED */
