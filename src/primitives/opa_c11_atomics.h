ccd1d2855f66405cbea7cac97b4d961aefffc5c2
Jeff Hammond <jeff.science@gmail.com>
2014-03-03 14:11:04 -0600

    finish off impl of existing features; removed emulation.  need to add missing stuff.

--- src/primitives/opa_c11_atomics.h
+++ src/primitives/opa_c11_atomics.h
@@ -1,6 +1,7 @@
 /* -*- Mode: C; c-basic-offset:4 ; indent-tabs-mode:nil ; -*- */
 /*  
  *  (C) 2008 by Argonne National Laboratory.
+ *  (C) 2014 by Argonne National Laboratory, Leadership Computing Facility.
  *      See COPYRIGHT in top-level directory.
  */
 
@@ -80,46 +81,32 @@ static _opa_inline int OPA_decr_and_test_int(OPA_int_t *ptr)
     return (1 == atomic_fetch_add_explicit(ptr, -1, memory_order_relaxed));
 }
 
-#define OPA_fetch_and_incr_int_by_faa OPA_fetch_and_incr_int
-#define OPA_fetch_and_decr_int_by_faa OPA_fetch_and_decr_int
-#define OPA_add_int_by_faa OPA_add_int
-#define OPA_incr_int_by_fai OPA_incr_int
-#define OPA_decr_int_by_fad OPA_decr_int
-
-
 static _opa_inline void *OPA_cas_ptr(OPA_ptr_t *ptr, void *oldv, void *newv)
 {
-    return __sync_val_compare_and_swap(&ptr->v, oldv, newv);
+    return atomic_compare_exchange_strong_explicit(ptr, &oldv, &newv, memory_order_relaxed);
 }
 
 static _opa_inline int OPA_cas_int(OPA_int_t *ptr, int oldv, int newv)
 {
-    return __sync_val_compare_and_swap(&ptr->v, oldv, newv);
+    return atomic_compare_exchange_strong_explicit(ptr, &oldv, &newv, memory_order_relaxed);
 }
 
-#ifdef SYNC_LOCK_TEST_AND_SET_IS_SWAP
 static _opa_inline void *OPA_swap_ptr(OPA_ptr_t *ptr, void *val)
 {
-    return __sync_lock_test_and_set(&ptr->v, val);
+    return atomic_exchange_explicit(ptr, val, memory_order_relaxed);
 }
 
 static _opa_inline int OPA_swap_int(OPA_int_t *ptr, int val)
 {
-    return __sync_lock_test_and_set(&ptr->v, val);
+    return atomic_exchange_explicit(ptr, val, memory_order_relaxed);
 }
 
-#else
-#define OPA_swap_ptr_by_cas OPA_swap_ptr
-#define OPA_swap_int_by_cas OPA_swap_int 
-#endif
-
-#define OPA_write_barrier()      __sync_synchronize()
-#define OPA_read_barrier()       __sync_synchronize()
-#define OPA_read_write_barrier() __sync_synchronize()
-#define OPA_compiler_barrier()   __asm__ __volatile__  ( ""  ::: "memory" )
-
-
-
-#include"opa_emulated.h"
+/* TODO: write/read use of release/acquire might be backwards. */
+#define OPA_write_barrier()      atomic_thread_fence(memory_order_release);
+#define OPA_read_barrier()       atomic_thread_fence(memory_order_acquire);
+#define OPA_read_write_barrier() atomic_thread_fence(memory_order_acq_rel);
+/* FYI: According to C11 7.17.4.2 NOTE 2, atomic_signal_fence acts as
+ *      a compiler barrier.  Note sure how memory_order affects this. */
+#define OPA_compiler_barrier()   atomic_signal_fence(memory_order_acq_rel);
 
 #endif /* OPA_C11_ATOMICS_H_INCLUDED */
